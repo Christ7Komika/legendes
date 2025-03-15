a9840264175cd6c4ea86fdef09e9ca197407521d
Christ Komika <christkomika7@gmail.com>
2025-03-15 23:42:36 +0100

    Mise a jour

--- src/components/cart/CartButton.tsx
+++ src/components/cart/CartButton.tsx
@@ -9,7 +9,7 @@ export default function CartButton() {
       {albums.length > 0 && (
         <a
           href="/panier"
-          className="right-8 bottom-[60px] fixed flex justify-center items-center bg-white rounded-full w-[60px] h-[60px]"
+          className="right-8 bottom-[60px] z-40 fixed flex justify-center items-center bg-white shadow-neutral-300/30 shadow-xl rounded-full w-[60px] h-[60px]"
         >
           <span className="relative flex justify-center items-center w-7 h-7">
             <span className="-top-1.5 -right-2 absolute flex justify-center items-center bg-black border-2 border-white rounded-full w-5 h-5 text-[8px] text-white">

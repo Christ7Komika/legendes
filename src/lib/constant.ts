8c140c2b060587d16182751c15c598c93d9f93f3
Christ Komika <christkomika7@gmail.com>
2025-03-15 14:27:41 +0100

    Mise en ligne

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,7 +1,5 @@
 // export const HOST = "http://localhost:3000"
 // export const SERVER_HOST = "http://localhost:5000"
-export const STRIPE_PUBLIC_KEY = "pk_test_51NuC0OCGWW6egWyqrWRjHnDdxtbVFk9PKoVHncWzWDaAmFVN9Pw3R8QpdwRxIT2D8cJoWXQNnYvk0E2DZkZlqz9f001ypxdLTI"
 export const HOST = "https://legendes.vercel.app"
 export const SERVER_HOST = "https://legendes-api.onrender.com"
-// export const STRIPE_PUBLIC_KEY = "pk_live_51R0Xu9BIcJcr3Lpsg9ErNHUr7c8KXO2uo4mBrnLdz4JphOsuxAyW7cO3TNDhjZfPYLsgXC5We1wSwrq2sWn6g5es000RQqNU4f"
 export const NAVBAR_SCROLL_CHANGE = 140;
--- src/lib/stripe.ts
+++
@@ -1,6 +0,0 @@
-import { loadStripe } from '@stripe/stripe-js';
-import { STRIPE_PUBLIC_KEY } from './constant';
-
-export const stripe = loadStripe(STRIPE_PUBLIC_KEY, {
-    locale: "fr-FR"
-});
